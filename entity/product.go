f705c497a6f91b474748fb5caa9ef484c3d809a9
Yonathan Kevin <yonathan.kevin20@gmail.com>
2021-12-21 10:35:46 +0700

    function template

--- entity/product.go
+++ entity/product.go
@@ -1,8 +1,24 @@
 package entity
 
+import "strconv"
+
 type Product struct {
 	Id    int
 	Name  string
 	Price int
 	Stock int
+}
+
+func (product Product) StockStatus() (status string) {
+	if product.Stock < 3 {
+		status = "Stock is running out"
+	} else if product.Stock < 10 {
+		status = "Stock is limited"
+	} else if product.Stock == 0 {
+		status = "Out of stock"
+	} else {
+		status = "Stock: " + strconv.Itoa(product.Stock)
+	}
+
+	return
 }
